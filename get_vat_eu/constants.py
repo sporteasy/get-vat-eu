3eda491d382beee1d41d8b09c7aac3b37ef46294
Franco Masotti <franco.masotti@live.com>
2019-10-07 11:41:03 +0200

    Fixed URL.

--- get_vat_eu/constants.py
+++ get_vat_eu/constants.py
@@ -26,7 +26,7 @@ common_defaults = dict()
 
 urls = dict()
 urls[
-    'ec_check_vat_api'] = 'http://ec.europa.eu/taxation_customs/vies/checkVatTestService.wsdl'
+    'ec_check_vat_api'] = 'http://ec.europa.eu/taxation_customs/vies/checkVatService.wsdl'
 
 vat = dict()
 vat['regex'] = "((ATU|DK|FI|HU|LU|MT|SI)[0-9]{8}|(BE|BG)[0-9]{9,10}|(ES([0-9]|[A-Z])[0-9]{7}([A-Z]|[0-9]))|(HR|IT|LV)[0-9]{11}|CY[0-9]{8}[A-Z]|CZ[0-9]{8,10}|(DE|EE|EL|GB|PT)[0-9]{9}|FR[A-Z0-9]{2}[0-9]{8}[A-Z0-9]|IE[0-9]{7}[A-Z0-9]{2}|LT[0-9]{9}([0-9]{3})?|NL[0-9]{9}B([0-9]{2})|PL[0-9]{10}|RO[0-9]{2,10}|SK[0-9]{10}|SE[0-9]{12})"
